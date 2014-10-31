c44f8225cf422e706f4ad65c8eec16c5187aaf11
Alexander Alemayhu <alexander@bitraf.no>
2014-10-31 18:50:00 +0100

    o Remove annoying comments.

--- 電卓/AppDelegate.h
+++ 電卓/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  電卓
-//
-//  Created by Alexander Alemayhu on 31/10/14.
-//  Copyright (c) 2014 Alexander A. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
--- 電卓/ViewController.m
+++ 電卓/ViewController.m
@@ -1,11 +1,3 @@
-//
-//  ViewController.m
-//  電卓
-//
-//  Created by Alexander Alemayhu on 31/10/14.
-//  Copyright (c) 2014 Alexander A. All rights reserved.
-//
-
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -16,12 +8,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
