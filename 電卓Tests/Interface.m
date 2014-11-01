6222225fc06fd93d68c0f6cf34bea677aea4da2d
Alexander Alemayhu <alexander.alemayhu@googlemail.com>
2014-11-01 12:40:08 +0100

    Refactoring interface test.
    o Split into smaller tests.

--- 電卓Tests/Interface.m
+++ 電卓Tests/Interface.m
@@ -5,6 +5,7 @@
 
 @interface Interface : XCTestCase {
     
+    NSString *textFieldExpectedText;
     ViewController *vc;
 }
 
@@ -15,42 +16,21 @@
 - (void)setUp {
     [super setUp];
     
-    /*
-     
-     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-     self.vc = [storyboard instantiateViewControllerWithIdentifier:@"main"];
-     [self.vc performSelectorOnMainThread:@selector(loadView) withObject:nil waitUntilDone:YES];
-     
-     */
+    textFieldExpectedText = @"";
     
     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
     vc = [storyboard instantiateViewControllerWithIdentifier:@"ViewController"];
+    
+    // Make sure viewDidLoad is called
+    [vc performSelectorOnMainThread:@selector(loadView) withObject:nil waitUntilDone:YES];
 }
 
 - (void)tearDown {
     [super tearDown];
 }
 
-- (void)testView {
-    
-    // Make sure viewDidLoad is called
-    [vc performSelectorOnMainThread:@selector(loadView) withObject:nil waitUntilDone:YES];
-
-    // Check our tags
-    XCTAssertEqual(vc.answer_but.tag, ANSWER);
-    XCTAssertEqual(vc.add_but.tag, ADD);
-    XCTAssertEqual(vc.but_1.tag, DIGIT_ONE);
-    XCTAssertEqual(vc.but_2.tag, DIGIT_TWO);
-    XCTAssertEqual(vc.but_3.tag, DIGIT_THREE);
-    XCTAssertEqual(vc.but_4.tag, DIGIT_FOUR);
-    XCTAssertEqual(vc.but_5.tag, DIGIT_FIVE);
-    XCTAssertEqual(vc.but_6.tag, DIGIT_SIX);
-    XCTAssertEqual(vc.but_7.tag, DIGIT_SEVEN);
-    XCTAssertEqual(vc.but_8.tag, DIGIT_EIGHT);
-    XCTAssertEqual(vc.but_9.tag, DIGIT_NINE);
-    
+-(void) testPressing {
     // Test all our button actions
-    NSString *textFieldExpectedText;
     for (int tag = DIGIT_ONE; tag <= DIGIT_NINE; tag++) {
         UIButton *button = (UIButton *) [[vc view] viewWithTag:tag];
         [button sendActionsForControlEvents:UIControlEventTouchUpInside];
@@ -58,6 +38,9 @@
         XCTAssert([[[vc textField] text] isEqualToString:textFieldExpectedText], @"Missing action");
         [[vc textField] setText:@""];
     }
+}
+
+- (void)testFunctionality {
     
     // Let us see if 2+2 equals 4
     [[vc but_2] sendActionsForControlEvents:UIControlEventTouchUpInside];
@@ -86,6 +69,21 @@
     [[vc answer_but] sendActionsForControlEvents:UIControlEventTouchUpInside];
     textFieldExpectedText = @"6";
     XCTAssert([[[vc textField] text] isEqualToString:textFieldExpectedText], @"Wrong answer!");
+}
+
+-(void) testViewTags {
+    // Check our tags
+    XCTAssertEqual(vc.answer_but.tag, ANSWER);
+    XCTAssertEqual(vc.add_but.tag, ADD);
+    XCTAssertEqual(vc.but_1.tag, DIGIT_ONE);
+    XCTAssertEqual(vc.but_2.tag, DIGIT_TWO);
+    XCTAssertEqual(vc.but_3.tag, DIGIT_THREE);
+    XCTAssertEqual(vc.but_4.tag, DIGIT_FOUR);
+    XCTAssertEqual(vc.but_5.tag, DIGIT_FIVE);
+    XCTAssertEqual(vc.but_6.tag, DIGIT_SIX);
+    XCTAssertEqual(vc.but_7.tag, DIGIT_SEVEN);
+    XCTAssertEqual(vc.but_8.tag, DIGIT_EIGHT);
+    XCTAssertEqual(vc.but_9.tag, DIGIT_NINE);
 }
 
 @end
